0e3b0cb1af3307ac67b5ff6f4f87ea9f25fae79d
Alex Wyett <alex.wyett@gmail.com>
2015-11-24 16:47:12 +0000

    Added correct path & toArray

--- src/common/ContactMethodType.js
+++ src/common/ContactMethodType.js
@@ -1,9 +1,21 @@
 var SingleEntity = require('./SingleEntity');
 
 function ContactMethodType(contactMethodTypeId) {
-    this.path = 'contactmethodtype/' + contactMethodTypeId;
-    this.id = contactMethodTypeId;
+  this.path = 'contactmethodtype';
+  this.createPath = this.path;
+  this.id = contactMethodTypeId;
 }
 ContactMethodType.prototype = new SingleEntity();
+
+ContactMethodType.prototype.toArray = function() {
+    return {
+        'method': this.method,
+        'encoding': this.encoding.encoding,
+        'characterlimit': this.characterlimit,
+        'maximumaddresslength': this.maximumaddresslength,
+        'addressvalidationtype': this.addressvalidationtype,
+        'addressvalidation': this.addressvalidation
+    };
+};
 
 module.exports = ContactMethodType;
