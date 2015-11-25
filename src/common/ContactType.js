2b4ec35d9dd0926ba491c0d870d259881b309f90
Alex Wyett <alex.wyett@gmail.com>
2015-11-25 10:40:54 +0000

    Added toArray method.

--- src/common/ContactType.js
+++ src/common/ContactType.js
@@ -1,9 +1,16 @@
 var SingleEntity = require('./SingleEntity');
 
-function ContactType(contactTypeId) {
-    this.path = 'contacttype/' + contactTypeId;
-    this.id = contactTypeId;
+function ContactType(id) {
+    this.path = 'contacttype';
+    this.createPath = this.path;
+    this.id = id;
 }
 ContactType.prototype = new SingleEntity();
+
+ContactType.prototype.toArray = function() {
+    return {
+        'type': this.type
+    };
+};
 
 module.exports = ContactType;
