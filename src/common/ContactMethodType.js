44a566790ea3d1249784e723a0f27e7a9a9cd12b
Alex Wyett <alex.wyett@gmail.com>
2015-11-27 16:11:00 +0000

    Correct toArray methods

--- src/common/ContactMethodType.js
+++ src/common/ContactMethodType.js
@@ -1,15 +1,31 @@
 var SingleEntity = require('./SingleEntity');
+var Encoding = require('./Encoding');
 
 function ContactMethodType(contactMethodTypeId) {
   this.path = 'contactmethodtype';
   this.createPath = this.path;
   this.id = contactMethodTypeId;
+  this.characterlimit = 0;
+  this.maximumaddresslength = '';
+  this.addressvalidationtype = '';
+  this.addressvalidation = '';
+  this.encoding = new Encoding();
 }
 ContactMethodType.prototype = new SingleEntity();
 
 ContactMethodType.prototype.toArray = function() {
     return {
         'method': this.method,
+        'encoding': this.encoding.encoding,
+        'characterlimit': this.characterlimit,
+        'maximumaddresslength': this.maximumaddresslength,
+        'addressvalidationtype': this.addressvalidationtype,
+        'addressvalidation': this.addressvalidation
+    };
+};
+
+ContactMethodType.prototype.toUpdateArray = function() {
+    return {
         'encoding': this.encoding.encoding,
         'characterlimit': this.characterlimit,
         'maximumaddresslength': this.maximumaddresslength,
--- src/common/MarketingBrand.js
+++ src/common/MarketingBrand.js
@@ -1,9 +1,25 @@
 var SingleEntity = require('./SingleEntity');
+var Agency = require('./Agency');
+var BookingBrand = require('./BookingBrand');
 
 function MarketingBrand(id) {
     this.path = 'marketingbrand';
+    this.createPath = 'marketingbrand';
     this.id = id;
+    this.agency = new Agency();
+    this.defaultbookingbrand = new BookingBrand();
 }
 MarketingBrand.prototype = new SingleEntity();
+
+MarketingBrand.prototype.toArray = function() {
+  return {
+    marketingbrandcode: this.code,
+    marketingbrand: this.name,
+    website: this.website,
+    email: this.email,
+    agencyid: this.agency.id,
+    defaultbookingbrandid: this.defaultbookingbrand.id
+  };
+};
 
 module.exports = MarketingBrand;
