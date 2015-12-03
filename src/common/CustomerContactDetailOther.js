b5a4d5a769b7f162ce9a3c056c8c0073805043f9
Mark <mark@marksharratt.net>
2015-12-03 14:36:41 +0000

    Add customer contact detail

--- src/common/CustomerContactDetailOther.js
+++ src/common/CustomerContactDetailOther.js
@@ -2,6 +2,7 @@ var SingleEntity = require('./SingleEntity');
 
 function CustomerContactDetailOther(cust_id, contact_id) {
     this.path = 'customer/'+cust_id+'/contactdetailother';
+    this.createPath = 'customer/'+cust_id+'/contactdetailother';
     this.id = contact_id;
 }
 CustomerContactDetailOther.prototype = new SingleEntity();
@@ -13,5 +14,14 @@ CustomerContactDetailOther.prototype.toArray = function() {
         value: this.value
     };
 };
+
+CustomerContactDetailOther.prototype.toCreateArray = function() {
+    return {
+        contactmethodsubtype: this.contactmethodsubtype,
+        contactmethodtype: this.contactmethodtype,
+        value: this.value
+    };
+};
+
 
 module.exports = CustomerContactDetailOther;
