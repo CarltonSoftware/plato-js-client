3163df4f98a14ec0144ce4a3156e5a724128b7b1
Alex Wyett <alex.wyett@gmail.com>
2015-12-18 13:24:52 +0000

    Added createpath

--- src/common/BrandingGroup.js
+++ src/common/BrandingGroup.js
@@ -1,9 +1,20 @@
 var SingleEntity = require('./SingleEntity');
+var Agency = require('./Agency');
 
 function BrandingGroup(id) {
-    this.path = 'brandinggroup';
-    this.id = id;
+  this.path = 'brandinggroup';
+  this.createPath = 'brandinggroup';
+  this.id = id;
+  this.agency = new Agency();
 }
+
 BrandingGroup.prototype = new SingleEntity();
+BrandingGroup.prototype.toArray = function() {
+  return {
+    brandinggroupcode: this.code,
+    brandinggroup: this.name,
+    agencyid: this.agency.id
+  };
+};
 
 module.exports = BrandingGroup;
