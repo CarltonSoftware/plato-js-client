995602310f2a486e520f6faf6bc6b7156cbe6243
Alex Wyett <alex.wyett@gmail.com>
2016-02-17 10:31:44 +0000

    Path now simplified. If when intstantiating object you need to update, a parent Actor instance must be applied.

--- src/common/ActorContactDetailOther.js
+++ src/common/ActorContactDetailOther.js
@@ -1,27 +1,19 @@
 var SingleEntity = require('./SingleEntity');
 
-function ActorContactDetailOther(actor, cust_id, contact_id) {
-    this.path = actor+'/'+cust_id+'/contactdetailother';
-    this.createPath = actor+'/'+cust_id+'/contactdetailother';
-    this.id = contact_id;
+function ActorContactDetailOther(contact_id) {
+  this.path = 'contactdetailother';
+  this.createPath = this.path;
+  this.id = contact_id;
 }
+
 ActorContactDetailOther.prototype = new SingleEntity();
 ActorContactDetailOther.prototype.toArray = function() {
-    return {
-        id: this.id,
-        contactmethodsubtype: this.contactmethodsubtype,
-        contactmethodtype: this.contactmethodtype,
-        value: this.value
-    };
+  return {
+    id: this.id,
+    contactmethodsubtype: this.contactmethodsubtype,
+    contactmethodtype: this.contactmethodtype,
+    value: this.value
+  };
 };
-
-ActorContactDetailOther.prototype.toCreateArray = function() {
-    return {
-        contactmethodsubtype: this.contactmethodsubtype,
-        contactmethodtype: this.contactmethodtype,
-        value: this.value
-    };
-};
-
 
 module.exports = ActorContactDetailOther;
