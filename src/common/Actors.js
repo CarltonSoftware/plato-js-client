9e7fb40f12497ea9ffce924a17391c26901347ab
Alex Wyett <alex.wyett@gmail.com>
2016-02-17 10:32:40 +0000

    Formatting

--- src/common/Actors.js
+++ src/common/Actors.js
@@ -3,13 +3,14 @@ var Collection = require('./Collection');
 
 
 function Actors(actor, page, limit) {
-    this.path = actor;
-    this.options.path = actor;
-    this.options.object = Actor;
-    
-    this.page = typeof page !== 'undefined' ? page : null;
-    this.limit = typeof limit !== 'undefined' ? limit : null;
+  this.path = actor;
+  this.options.path = actor;
+  this.options.object = Actor;
+  
+  this.page = typeof page !== 'undefined' ? page : null;
+  this.limit = typeof limit !== 'undefined' ? limit : null;
 }
+
 Actors.prototype = new Collection();
 
 module.exports = Actors;
