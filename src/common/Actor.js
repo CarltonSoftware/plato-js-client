4f536ff4189a26bc4057cd0d7b1539fc3a3671dd
Alex Wyett <alex.wyett@gmail.com>
2016-02-17 10:30:33 +0000

    Added more sub entities into Actor

--- src/common/Actor.js
+++ src/common/Actor.js
@@ -2,34 +2,42 @@ var SingleEntity = require('./SingleEntity');
 var Collection = require('./Collection');
 var CustomerDocument = require('./CustomerDocument');
 var Note = require('./Note');
+var Language = require('./Language');
+var BankAccount = require('./BankAccount');
+var ActorContactDetailOther = require('./ActorContactDetailOther');
+var ActorContactDetailAddress = require('./ActorContactDetailAddress');
 
-function Actor(actor, id) {
-    this.path = actor;
-    this.createPath = actor;
-    this.id = id;
-    this.notes = new Collection({object: Note});
-    this.documents = new Collection({object: CustomerDocument});
-    this.duplicates = new Collection({object: Actor});
+function Actor(id) {
+  this.id = id;
+  this.notes = new Collection({ object: Note });
+  this.documents = new Collection({ object: CustomerDocument });
+  this.duplicates = new Collection({ object: Actor });
+
+  this.language = new Language();
+  this.language.code = 'EN';
+  this.language.name = 'English';
+
+  this.bankaccounts = new Collection({ object: BankAccount, parent: this });
+  this.contactdetails = new Collection({
+    object: ActorContactDetailOther,
+    parent: this,
+    discriminator: 'type',
+    discriminatorMap: {
+      'C': ActorContactDetailOther,
+      'P': ActorContactDetailAddress
+    }
+  });
 }
+
 Actor.prototype = new SingleEntity();
 Actor.prototype.toArray = function() {
-    return {
-        id: this.id,
-        title: this.title,
-        firstname: this.firstname,
-        surname: this.surname
-    };
-};
-
-/* Hardcode language for now */
-Actor.prototype.toCreateArray = function() {
-    return {
-        title: this.title,
-        firstname: this.firstname,
-        surname: this.surname,
-        salutation: this.salutation,
-        languagecode: 'EN'
-    };
+  return {
+    id: this.id,
+    title: this.title,
+    firstname: this.firstname,
+    surname: this.surname,
+    languagecode: this.language.code
+  };
 };
 
 module.exports = Actor;
