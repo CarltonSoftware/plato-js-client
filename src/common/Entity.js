b185468d71748445a8f4400ad3b980ef243f147f
Pete Lawrence <pete@carltonsoftware.co.uk>
2016-02-25 12:29:11 +0000

    Temp hack to get images uploads working

--- src/common/Entity.js
+++ src/common/Entity.js
@@ -114,6 +114,7 @@ Entity.prototype.createPromiseResult = function(path, data) {
     result.then(function(res) {
       if (res.status.code === 201) {
         var newLocation = res.headers['Content-Location'].replace('/app_dev.php/v2', '');//TODO: remove the need for .replace(...)
+        newLocation = newLocation.replace('/v2', '');
         client.get({ path: newLocation}).then(function(res) {
           resolve(e.mutateResponse(res.entity));
         }, function(res) {
@@ -141,6 +142,7 @@ Entity.prototype.uploadPromiseResult = function(path, data) {
     result.then(function(res) {
       if (res.status.code === 201) {
         var newLocation = res.headers['Content-Location'].replace('/app_dev.php/v2', '');//TODO: remove the need for .replace(...)
+        newLocation = newLocation.replace('/v2', '');
         client.get({ path: newLocation}).then(function(res) {
           resolve(e.mutateResponse(res.entity));
         }, function(res) {
