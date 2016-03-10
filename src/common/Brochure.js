aef39d253cb77ec9cc8abd87bf802ecafa2aaa0e
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-03-10 15:02:16 +0000

    Add Brochure and BrochureRequest

---
+++ src/common/Brochure.js
@@ -0,0 +1,24 @@
+var SingleEntity = require('./SingleEntity');
+var MarketingBrand = require('./MarketingBrand');
+
+function Brochure(id) {
+  this.path = this.createPath = 'brochure';
+  this.id = id;
+  this.marketingbrand = new MarketingBrand;
+}
+
+Brochure.prototype = new SingleEntity();
+Brochure.prototype.toArray = function() {
+  return {
+    name: this.name,
+    marketingbrandcode: this.marketingbrand.code,
+    year: this.year,
+    orderfromdate: this.orderfromdate,
+    ordertodate: this.ordertodate,
+    availablefromdate: this.availablefromdate,
+    weight: this.weight,
+    cost: this.cost
+  };
+};
+
+module.exports = Brochure;
