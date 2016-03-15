cfcc2a30212c6d17d6c4414a076fcc9de098f555
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-03-15 10:52:39 +0000

    Add customer to brochure request

--- src/common/BrochureRequest.js
+++ src/common/BrochureRequest.js
@@ -1,10 +1,12 @@
 var SingleEntity = require('./SingleEntity');
 // var SourceMarketingBrand = require('./SourceMarketingBrand');
+var Customer = require('./Customer');
 
 function BrochureRequest(id) {
-  this.path = 'request';
+  this.path = this.createPath = 'request';
   this.id = id;
   // this.sourcemarketingbrand = new SourceMarketingBrand;
+  this.customer = new Customer;
 }
 
 BrochureRequest.prototype = new SingleEntity();
@@ -12,8 +14,10 @@ BrochureRequest.prototype.toArray = function() {
   return {
     customerid: this.customer.id,
     emailoptin: this.emailoptin,
+    processeddatetime: this.processeddatetime
+    // requestdatetime:
     // sourcemarketingbrandid: this.sourcemarketingbrand.id,
-    // tabsuserid: // optional
+    // tabsuserid: this.tabsuser.id,
   };
 };
 
