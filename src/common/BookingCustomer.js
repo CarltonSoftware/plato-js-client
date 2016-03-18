a0adb63c5c433e732c680e119ee7476f43a31bad
Mark <mark@marksharratt.net>
2016-03-18 12:16:43 +0000

    Add booking customer

--- src/common/BookingCustomer.js
+++ src/common/BookingCustomer.js
@@ -2,16 +2,16 @@ var SingleEntity = require('./SingleEntity');
 var Customer = require('./Customer');
 
 function BookingCustomer(bookingId, id) {
-    this.path = 'booking/' + bookingId + '/customer';
-    this.createPath = 'booking/' + bookingId + '/customer';
-    this.id = id;
-    this.details = new Customer;
+  this.path = 'booking/' + bookingId + '/customer';
+  this.createPath = 'booking/' + bookingId + '/customer';
+  this.id = id;
+  this.details = new Customer;
 }
 BookingCustomer.prototype = new SingleEntity();
 
 BookingCustomer.prototype.toArray = function() {
   return {
-    customerid: this.details.id,
+    customerid: this.customerid ? this.customerid : this.details.id,
   };
 }
 
