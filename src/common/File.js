0b7c4d1fc0ea4bcaf4c697607e11cbcb65487faf
Alex Wyett <alex.wyett@gmail.com>
2016-04-01 12:16:02 +0100

    Fixed path issue

--- src/common/File.js
+++ src/common/File.js
@@ -3,6 +3,7 @@ var client = require('./platoJsClient').getInstance();
 
 function File(id) {
   this.path = 'file';
+  this.createPath = this.path;
   this.id = id;
 }
 File.prototype = new SingleEntity();
@@ -10,7 +11,7 @@ File.prototype.okPromiseResult = function(path, params) {
   var self = this;
   return new Promise(function(resolve, reject) {
     var request = new XMLHttpRequest();
-    request.open('GET', client.getHost() + '/' + path);
+    request.open('GET', client.getHost() + path);
     request.responseType = 'blob';
     request.setRequestHeader('Authorization', 'Bearer Y2ViNDM2NTc0NTMwYjljYWMwYzExMzIxZGE0ZjdlYmE3MjgwNmMxMzRlNzVhOTcyMGU1MjE0M2I2Njc0ZjcxZQ');
     request.onreadystatechange = function(e) {
