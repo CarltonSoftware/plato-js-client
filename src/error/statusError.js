660e8484e5ddfd29a903367eb8a11238100fe2d2
Alex Wyett <alex.wyett@gmail.com>
2016-04-01 12:36:24 +0100

    Added a bit more context into the status error message.

--- src/error/statusError.js
+++ src/error/statusError.js
@@ -1,8 +1,16 @@
 function statusError(response) {
-    this.name = 'statusError';
-    this.message = 'Entity not found';
-    this.statusCode = response.status.code;
-}
+  this.name = 'statusError';
+  this.message = 'Entity not found';
+  this.statusCode = response.status.code;
+
+  if (typeof response.entity === 'object' && response.entity.hasOwnProperty('errorDescription')) {
+    var error = JSON.parse(response.entity.errorDescription);
+    if (error) {
+      this.message = error.description;
+    }
+  }
+};
+
 statusError.prototype = Error.prototype;
 
 module.exports = statusError;
