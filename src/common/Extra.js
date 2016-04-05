aea2a94ff1344dbe593c9a9ef37154f565f5abba
Alex Wyett <alex.wyett@gmail.com>
2016-04-05 11:55:33 +0100

    Formatting

--- src/common/Extra.js
+++ src/common/Extra.js
@@ -3,25 +3,18 @@ var StaticCollection = require('./StaticCollection');
 var Branding = require('./Branding');
 
 function Extra(id) {
-    this.path = 'extra';
-    this.createPath = 'extra';
-    this.id = id;
-    this.brandings = new StaticCollection({object: Branding});
+  this.path = 'extra';
+  this.createPath = 'extra';
+  this.id = id;
+  this.brandings = new StaticCollection({
+    object: Branding
+  });
 }
-Extra.prototype = new SingleEntity();
 
-Extra.prototype.toCreateArray = function() {
+Extra.prototype = new SingleEntity();
+Extra.prototype.toArray = function() {
   return {
     //TODO: Add in the fields necessary to create a Extra
-    extracode: this.extracode,
-    extratype: this.extratype,
-    description: this.description
-  };
-};
-
-Extra.prototype.toUpdateArray = function() {
-  return {
-    //TODO: Add in the fields necessary to update a Extra
     extracode: this.extracode,
     extratype: this.extratype,
     description: this.description
