a732227d300d7d0fbfc9c70ef4ed9028f7dbc203
Pete Lawrence <petejlawrence@icloud.com>
2016-06-03 11:09:07 +0100

    Revert "Removed OAuth token from platoJsClient.js.  Previously when calling setInstance you could set 'defaultToken' to be true to use the built in token, now pass the token that you want to use in as the 'defaultToken' option"
    This reverts commit 980f612d7202b1aef4fe52d365539aa491756404.

--- src/PlatoJsClient.js
+++ src/PlatoJsClient.js
@@ -1,7 +1,7 @@
 var platoJsClient = require('./');
 var client = platoJsClient.client.getInstance().setInstance({
 	apiRoot: 'http://docker.carltonsoftware.co.uk:49158/app_dev.php/v2',
-	defaultToken: '<OAuth Token to go here for testing>'
+	defaultToken: true
 });
 
 var collection = new platoJsClient.FilterCollection({
