0f8448f681d120d1f4ea51189147be8df9a3f5bc
jonbev <jon@carltonsoftware.co.uk>
2016-06-10 15:57:30 +0100

    Update contact after api change

--- src/common/Contact.js
+++ src/common/Contact.js
@@ -20,6 +20,13 @@ Contact.prototype.toCreateArray = function() {
   return {
     contacttype: this.contacttype,
     contactmethodtype: this.contactmethodtype,
+    subject: this.subject,
+    sender: this.sender,
+    status_status: this.status_status,
+    status_statusdatetime: this.status_statusdatetime,
+    status_intermediary: this.status_intermediary,
+    status_reference: this.status_reference,
+    status_detail: this.status_detail,
     contactdatetime: this.contactdatetime,
     content: this.content,
     deleted: false
