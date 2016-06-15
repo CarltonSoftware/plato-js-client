2e51e356f039108235bb8af6a79f64bce21872ea
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-06-15 14:24:56 +0100

    Actor potential duplicate changes

--- src/common/PotentialDuplicate.js
+++ src/common/PotentialDuplicate.js
@@ -2,20 +2,20 @@ var SingleEntity = require('./SingleEntity');
 var EntityLink = require('./EntityLink');
 
 function PotentialDuplicate(id) {
+  var Actor = require('./Actor');
+
   this.path = 'potentialduplicate';
   this.createPath = 'potentialduplicate';
   this.id = id;
-  this.actor1 = new EntityLink({
-    entity: 'Customer'
-  });
-  this.actor2 = new EntityLink({
-    entity: 'Customer'
-  });
+  this.actor1 = new Actor();
+  this.actor2 = new Actor();
+  this.processedby = new EntityLink({ entity: 'TabsUser' });
 }
 PotentialDuplicate.prototype = new SingleEntity();
 PotentialDuplicate.prototype.toArray = function() {
   return {
-
+    actorkept: this.actorkept,
+    processedbytabsuserid: this.processedby.id
   };
 };
 
