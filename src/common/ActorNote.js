e65521c77a38c3e18ffd972dac5c83950df4ff7c
Alex Wyett <alex@carltonsoftware.co.uk>
2016-07-07 15:05:57 +0100

    added entity link

--- src/common/ActorNote.js
+++ src/common/ActorNote.js
@@ -1,23 +1,20 @@
 var SingleEntity = require('./SingleEntity');
 var Note = require('./Note');
+var EntityLink = require('./EntityLink');
 
 function ActorNote(id) {
   this.createPath = 'actornote';
   this.path = 'actornote';
   this.id = id;
   this.note = new Note();
+  this.actor = new EntityLink({
+    entity: 'Actor'
+  });
 }
 
 ActorNote.prototype = new SingleEntity();
 
 ActorNote.prototype.toArray = function() {
-  return {
-    actorid: this.actor.id,
-    noteid: this.note.id,
-  };
-};
-
-ActorNote.prototype.toCreateArray = function() {
   return {
     actorid: this.actor.id,
     noteid: this.note.id,
