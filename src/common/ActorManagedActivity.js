c32adad6c38e10ab9fd6e1197a46c649cb172931
Alex Wyett <alex@carltonsoftware.co.uk>
2016-07-12 14:57:30 +0100

    Fix path

--- src/common/ActorManagedActivity.js
+++ src/common/ActorManagedActivity.js
@@ -1,15 +1,20 @@
 var SingleEntity = require('./SingleEntity');
 var ManagedActivity = require('./ManagedActivity');
 
-function ActorManagedActivity(actorId, id) {
-    // Set the actor path explicitly to fix the route, otherwise the actor type is used.
-    this.path = 'actor/' + actorId + '/managedactivity';
-    this.createPath = this.path;
-    this.id = id;
-
-    this.managedactivity = new ManagedActivity();
+function ActorManagedActivity(id) {
+  this.path = 'managedactivity';
+  this.createPath = this.path;
+  this.id = id;
+  this.managedactivity = new ManagedActivity();
 }
+
 ActorManagedActivity.prototype = new SingleEntity();
+ActorManagedActivity.prototype.getCreatePath = function() {
+  return ['actor', this.parent.id, this.path].join('/');
+};
+ActorManagedActivity.prototype.getUpdatePath = function() {
+  return ['actor', this.parent.id, this.path, this.id].join('/');
+};
 
 ActorManagedActivity.prototype.toArray = function() {
   return {
