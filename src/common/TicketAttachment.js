f86389d0759eecc5f243d4780c3398f349929c01
Alex Wyett <alex@carltonsoftware.co.uk>
2016-07-20 12:32:26 +0100

    Handle ticket attachments

--- src/common/TicketAttachment.js
+++ src/common/TicketAttachment.js
@@ -1,5 +1,6 @@
 var SingleEntity = require('./SingleEntity');
 var File = require('./File');
+var client = require('./platoJsClient').getInstance();
 
 function TicketAttachment(id) {
   this.path = 'attachment';
@@ -7,17 +8,38 @@ function TicketAttachment(id) {
   this.id = id;
   this.filedata = new File();
 }
+
 TicketAttachment.prototype = new SingleEntity();
 TicketAttachment.prototype.toArray = function() {
   return {
     filename: this.filename
   };
 };
+
 TicketAttachment.prototype.toFormData = function() {
   var formData = new FormData();
-  formData.append('filedata', this.filedata);
+  formData.append('data', this.filedata);
   formData.append('filename', this.filename);
   return formData;
+};
+
+TicketAttachment.prototype.getData = function() {
+  var result = client.get({ 
+    path: this.getUpdatePath() + '/data',
+    mixin: { responseType: 'blob' }
+  });
+
+  return new Promise(function(resolve, reject) {
+    result.then(function(res) {
+      if (res.status.code === 200) {
+        self.data = res.entity;
+        self.url = URL.createObjectURL(res.entity);
+        resolve(self);
+      } else {
+        reject(new statusError(res));
+      }
+    });
+  });
 };
 
 module.exports = TicketAttachment;
--- src/common/TicketMessage.js
+++ src/common/TicketMessage.js
@@ -1,16 +1,15 @@
 var SingleEntity = require('./SingleEntity');
 
-function TicketMessage(ticketId, id) {
-  this.path = 'ticket/' + ticketId + '/message';
-  this.createPath = 'ticket/' + ticketId + '/message';
+function TicketMessage(id) {
+  this.path = 'message';
+  this.createPath = 'message';
   this.id = id;
 }
+
 TicketMessage.prototype = new SingleEntity();
 TicketMessage.prototype.toArray = function() {
   return {
-    tabsuser: this.tabsuser,
-    message: this.message,
-    createddate: this.createddate
+    message: this.message
   };
 };
 
