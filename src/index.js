3d060f3e0fd49a07c85eb401ab624d8e58e1a276
Mark <mark@marksharratt.net>
2016-08-17 13:56:42 +0100

    Looks like something odd happened with a merge. reinstated deleted lines

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ module.exports.common.Owner = require('./common/Owner');
 module.exports.common.Office = require('./common/Office');
 module.exports.common.Supplier = require('./common/Supplier');
 module.exports.common.SupplierServiceCharge = require('./common/SupplierServiceCharge');
+module.exports.common.SupplierServiceEvent = require('./common/SupplierServiceEvent');
 
 // Property Related Objects
 module.exports.common.Properties = require('./common/Properties');
@@ -110,6 +111,7 @@ module.exports.common.Booking = require('./common/Booking');
 module.exports.common.BookingCustomer = require('./common/BookingCustomer');
 module.exports.common.BookingEnquiry = require('./common/BookingEnquiry');
 module.exports.common.BookingExtra = require('./common/BookingExtra');
+module.exports.common.BookingEvent = require('./common/BookingEvent');
 module.exports.common.BookingDocument = require('./common/BookingDocument');
 module.exports.common.BookingGuest = require('./common/BookingGuest');
 module.exports.common.BookingNote = require('./common/BookingNote');
@@ -145,6 +147,8 @@ module.exports.common.NoteType = require('./common/NoteType');
 module.exports.common.NoteActor = require('./common/NoteActor');
 
 // Payment Related Objects
+module.exports.common.ActorPaymentRefund = require('./common/ActorPaymentRefund');
+module.exports.common.BookingRefund = require('./common/BookingRefund');
 module.exports.common.PaymentMethod = require('./common/PaymentMethod');
 
 // Search
