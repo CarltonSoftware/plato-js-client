809a9b8f1e41c01beb400195e854f21581d088ea
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-09-01 13:29:18 +0100

    Add extra configuration validation

--- src/common/ExtraBrandingConfiguration.js
+++ src/common/ExtraBrandingConfiguration.js
@@ -1,10 +1,11 @@
+var Joi = require('joi');
 var SingleEntity = require('./SingleEntity');
 var VatBand = require('./VatBand');
 
-function ExtraBrandingConfiguration(extraId, brandingId) {
+function ExtraBrandingConfiguration(extraId, brandingId, id) {
   this.path = 'extra/' + extraId + '/branding/' + brandingId + '/configuration';
   this.createPath = this.path;
-  this.id = 0;
+  this.id = id;
   this.vatband = new VatBand();
 }
 ExtraBrandingConfiguration.prototype = new SingleEntity();
@@ -26,6 +27,7 @@ ExtraBrandingConfiguration.prototype.toCreateArray = function() {
     priceoverrideallowed: this.priceoverrideallowed ? 'true' : 'false',
     quantityoverrideallowed: this.quantityoverrideallowed? 'true' : 'false',
     type: this.type,
+    propertyid: this.propertyid
   };
 };
 
@@ -48,5 +50,28 @@ ExtraBrandingConfiguration.prototype.toUpdateArray = function() {
     type: this.type,
   };
 };
+
+ExtraBrandingConfiguration.validSchema = Joi.object().keys({
+  fromdate: Joi.string().required().label('from date'),
+  todate: Joi.string().required().label('to date'),
+  compulsory: Joi.boolean().required(),
+  included: Joi.boolean().required(),
+  payagency: Joi.boolean().required().label('pay agency'),
+  payowner: Joi.boolean().required().label('pay owner'),
+  visibletoowner: Joi.boolean().required().label('visible to owner'),
+  vatband: Joi.string().required().label('VAT band'),
+  visibletocustomer: Joi.boolean().required().label('visible to customer'),
+  customerselectable: Joi.boolean().required().label('customer selectable'),
+  maximumquantity: Joi.string().required().label('maximum quantity'),
+  decimalplaces: Joi.string().required().label('decimal places'),
+  priceoverrideallowed: Joi.boolean().required().label('price override allowed'),
+  quantityoverrideallowed: Joi.boolean().required().label('quantity override allowed'),
+  type: Joi.string().required(),
+  propertyid: Joi.when('type', {
+    is: 'PropertyBrandExtraConfiguration',
+    then: Joi.number().required(),
+    otherwise: Joi.forbidden()
+  })
+}),
 
 module.exports = ExtraBrandingConfiguration;
