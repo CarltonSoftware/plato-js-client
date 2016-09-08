84dbe207b9c725a37e46929741548ae2fdbd2d1b
Mark <mark@marksharratt.net>
2016-09-08 09:14:11 +0100

    Added branding to extrabranding.

--- src/common/ExtraBranding.js
+++ src/common/ExtraBranding.js
@@ -1,12 +1,14 @@
 var SingleEntity = require('./SingleEntity');
 var ExtraBrandingConfiguration = require('./ExtraBrandingConfiguration');
 var Collection = require('./Collection');
+var Branding = require('./Branding');
 
 function ExtraBranding(extraId,id) {
   this.path = 'extra/' + extraId + '/branding';
   this.createPath = 'extra/' + extraId + '/branding';
   this.id = id;
 
+  this.branding = new Branding();
   this.brandingConfiguration = new Collection({
     object: ExtraBrandingConfiguration,
     path: 'configuration',
