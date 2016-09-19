09abfb93b501ad9407528c718e0af3ae423c952a
Mark <mark@marksharratt.net>
2016-09-19 13:23:18 +0100

    Extras validation

--- src/common/Extra.js
+++ src/common/Extra.js
@@ -1,4 +1,5 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 var StaticCollection = require('./StaticCollection');
 var Collection = require('./Collection');
 var Branding = require('./Branding');
@@ -16,11 +17,16 @@ function Extra(id) {
 Extra.prototype = new SingleEntity();
 Extra.prototype.toArray = function() {
   return {
-    //TODO: Add in the fields necessary to create a Extra
     extracode: this.extracode,
     extratype: this.extratype,
     description: this.description
   };
 };
+
+Extra.validSchema = Joi.object().keys({
+  extracode: Joi.string().required().label('extra code'),
+  extratype: Joi.string().required().label('extra type'),
+  description: Joi.string().required().label('description')
+}),
 
 module.exports = Extra;
