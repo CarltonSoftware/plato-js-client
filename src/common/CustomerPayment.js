785b1f40fb3018cc31284e562e93c1d7daea58ab
Mark <mark@marksharratt.net>
2016-09-20 15:45:49 +0100

    customer payment validation

--- src/common/CustomerPayment.js
+++ src/common/CustomerPayment.js
@@ -1,6 +1,7 @@
 var SingleEntity = require('./SingleEntity');
 var Currency = require('./Currency');
 var PaymentMethod = require('./PaymentMethod');
+var Joi = require('joi');
 
 function CustomerPayment(id) {
   this.path = 'payment';
@@ -25,5 +26,16 @@ CustomerPayment.prototype.toCreateArray = function() {
   };
   return array;
 };
+
+CustomerPayment.validSchema = Joi.object().keys({
+  paymentdatetime: Joi.string().optional().label('payment date time'),
+  reference: Joi.string().optional().allow('').label('reference'),
+  paymentmethodid: Joi.number().required().label('paymentmethodid'),
+  currencyid: Joi.number().required().label('currency'),
+  bookingid: Joi.number().optional().label('booking id'),
+  amount: Joi.number().required().label('amount'),
+  bookingamount: Joi.number().optional().label('booking amount'),
+  securitydepositamount: Joi.number().optional().label('security deposit amount')
+}),
 
 module.exports = CustomerPayment;
