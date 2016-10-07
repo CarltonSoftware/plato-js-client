bcb9dfe11c69ed10b2ddddf2b55b0fd07180dbda
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-10-07 15:31:13 +0100

    Add ReportParameter (with ability to convert from CamelCase to proper words) and ReportRun

--- src/common/Report.js
+++ src/common/Report.js
@@ -1,10 +1,16 @@
 var client = require('./platoJsClient').getInstance();
 var SingleEntity = require('./SingleEntity');
+var StaticCollection = require('./StaticCollection');
+var ReportParameter = require('./ReportParameter');
+var ReportRun = require('./ReportRun');
 
 function Report(id) {
   this.path = 'report';
   this.createPath = 'report';
   this.id = id;
+  this.parameters = new StaticCollection({
+    object: ReportParameter
+  });
 }
 Report.prototype = new SingleEntity();
 
@@ -12,6 +18,10 @@ Report.prototype.run = function(params) {
   return client.post({
     path: this.getUpdatePath() + '/run',
     entity: params
+  }).then(function(res) {
+    var reportRun = new ReportRun(res.id);
+    reportRun.mutateResponse(res.entity);
+    return reportRun;
   });
 };
 
---
+++ src/common/ReportParameter.js
@@ -0,0 +1,14 @@
+var SingleEntity = require('./SingleEntity');
+
+function ReportParameter() {
+}
+ReportParameter.prototype = new SingleEntity();
+
+/**
+ * Returns the parameter's name, converted from CamelCase to space-separated
+ */
+ReportParameter.prototype.toString = function() {
+  return this.name.replace(/([A-Z])/g, " $1");
+};
+
+module.exports = ReportParameter;
