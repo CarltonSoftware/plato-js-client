347a1327d39af0fee148e019bbc8722a76bed2df
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2016-10-12 14:32:12 +0100

    Add CronTask validSchema

--- src/common/CronTask.js
+++ src/common/CronTask.js
@@ -1,4 +1,5 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function CronTask(id) {
   this.path = this.createPath = 'crontask';
@@ -16,5 +17,17 @@ CronTask.prototype.toArray = function() {
     headers: this.headers
   };
 };
+
+CronTask.prototype.validSchema = function() {
+  return Joi.object().keys({
+    schedule: Joi.string().required().label('Schedule'),
+    type: Joi.string().required().label('Type'),
+    method: Joi.string().required().label('Method'),
+    url: Joi.string().required().label('URL'),
+    parameters: Joi.string().empty('').label('Parameters'),
+    headers: Joi.string().empty('').label('Headers')
+  });
+};
+
 
 module.exports = CronTask;
