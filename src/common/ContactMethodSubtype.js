9642cdaa4f7cf02294b26013be5523f287928e16
Alex Wyett <alex@carltonsoftware.co.uk>
2016-11-10 10:17:42 +0000

    Rename contactmethodsubtype

--- src/common/ContactMethodSubtype.js
+++ src/common/ContactMethodSubtype.js
@@ -1,16 +1,16 @@
 var SingleEntity = require('./SingleEntity');
 
-function ContactMethodSubType(id) {
+function ContactMethodSubtype(id) {
   this.path = 'contactmethodsubtype';
   this.createPath = this.path;
   this.id = id;
 }
-ContactMethodSubType.prototype = new SingleEntity();
 
-ContactMethodSubType.prototype.toArray = function() {
+ContactMethodSubtype.prototype = new SingleEntity();
+ContactMethodSubtype.prototype.toArray = function() {
     return {
         'contactmethodsubtype': this.contactmethodsubtype
     };
 };
 
-module.exports = ContactMethodSubType;
+module.exports = ContactMethodSubtype;
