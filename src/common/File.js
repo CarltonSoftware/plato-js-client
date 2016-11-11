068fafc17925e7d476dc853b21bfbe4cdc5cb0a6
Alex Wyett <alex@carltonsoftware.co.uk>
2016-11-11 16:16:59 +0000

    Added method for getting resized images.

--- src/common/File.js
+++ src/common/File.js
@@ -2,6 +2,7 @@ var SingleEntity = require('./SingleEntity');
 var client = require('./platoJsClient').getInstance();
 var statusError = require('./../error/statusError');
 var Promise = require('es6-promise').Promise;
+var client = require('./platoJsClient').getInstance();
 
 function File(id) {
   this.path = 'file';
@@ -10,6 +11,25 @@ function File(id) {
 }
 
 File.prototype = new SingleEntity();
+File.prototype.getImage = function(style, width, height) {
+  var self = this;
+  var result = client.get({
+    path: [this.getUpdatePath(), style, width, height].join('/'),
+    mixin: { responseType: 'blob' }
+  });
+
+  return new Promise(function(resolve, reject) {
+    result.then(function(res) {
+      if (res.status.code === 200) {
+        self.data = res.entity;
+        self.url = URL.createObjectURL(res.entity);
+        resolve(self);
+      } else {
+        reject(new statusError(res));
+      }
+    });
+  });
+};
 File.prototype.okPromiseResult = function(path, params) {
   var self = this;
   var result = client.get({ path: path, mixin: { responseType: 'blob' } });
