14cbee2672c5df07f6561b6bc9894e8e80f65dfb
Alex Wyett <alex@carltonsoftware.co.uk>
2016-11-30 11:36:30 +0000

    Rename to Category.  Add ActorCategory.

--- src/common/ActorCategory.js
+++ src/common/ActorCategory.js
@@ -1,21 +1,17 @@
 var SingleEntity = require('./SingleEntity');
+var Category = require('./Category');
 
 function ActorCategory(id) {
   this.path = 'category';
   this.createPath = 'category';
   this.id = id;
+  this.category = new Category();
 }
+
 ActorCategory.prototype = new SingleEntity();
 ActorCategory.prototype.toArray = function() {
   return {
-    category: this.category,
-    description: this.description,
-    bookingsminimum: this.bookingsminimum,
-    bookingsmaximum: this.bookingsmaximum,
-    andor: this.andor,
-    bookingvalueminimum: this.bookingvalueminimum,
-    bookingvaluemaximum: this.bookingvaluemaximum,
-    period: this.period
+    categoryid: this.category.id
   };
 };
 
---
+++ src/common/Category.js
@@ -0,0 +1,23 @@
+var SingleEntity = require('./SingleEntity');
+
+function Category(id) {
+  this.path = 'category';
+  this.createPath = 'category';
+  this.id = id;
+};
+
+Category.prototype = new SingleEntity();
+Category.prototype.toArray = function() {
+  return {
+    category: this.category,
+    description: this.description,
+    bookingsminimum: this.bookingsminimum,
+    bookingsmaximum: this.bookingsmaximum,
+    andor: this.andor,
+    bookingvalueminimum: this.bookingvalueminimum,
+    bookingvaluemaximum: this.bookingvaluemaximum,
+    period: this.period
+  };
+};
+
+module.exports = Category;
