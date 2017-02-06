2526637781acc5efb75635b365db7232bbe73548
Mark <mark@marksharratt.net>
2017-02-06 16:03:33 +0000

    TABS2-528. Add includeoffers parameter to availability

--- src/common/PropertyAvailability.js
+++ src/common/PropertyAvailability.js
@@ -1,12 +1,13 @@
 var Entity = require('./Entity');
 
-function PropertyAvailability(propertyId, brandId, fromDate, toDate, includeChangeDays) {
+function PropertyAvailability(propertyId, brandId, fromDate, toDate, includeChangeDays, includeOffers) {
   this.path = '/property/' + propertyId + '/branding/' + brandId + '/availability';
   this.createPath = this.path;
   this.params = {
     fromdate: fromDate,
     todate: toDate,
-    includechangedays: includeChangeDays
+    includechangedays: includeChangeDays,
+    includeoffers: includeOffers
   };
 }
 PropertyAvailability.prototype = new Entity();
