ed324838437c65fb925dcaa98e8cf400f12f00a6
Alex Wyett <alex@carltonsoftware.co.uk>
2017-02-22 16:52:30 +0000

    Handle workorder doc paths better.

--- src/common/WorkOrderDocument.js
+++ src/common/WorkOrderDocument.js
@@ -1,9 +1,9 @@
 var SingleEntity = require('./SingleEntity');
 var Document = require('./Document');
 
-function WorkOrderDocument(workOrderId, id) {
-  this.path = 'workorder/' + workOrderId + '/document';
-  this.createPath = 'workorder/' + workOrderId + '/document';
+function WorkOrderDocument(id) {
+  this.path = 'document';
+  this.createPath = 'document';
   this.document = new Document();
   this.id = id;
 }
