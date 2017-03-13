b81b96f975febf09cb58253adf39ec40be69f2a2
Alex Wyett <alex@carltonsoftware.co.uk>
2017-03-13 09:16:09 +0000

    TABS2-751

--- src/common/ContactPreference.js
+++ src/common/ContactPreference.js
@@ -1,12 +1,10 @@
 var SingleEntity = require('./SingleEntity');
-var EntityLink = require('./EntityLink');
 var RoleReason = require('./RoleReason');
 
 function ContactPreference(id) {
   this.path = 'contactpreference';
   this.createPath = this.path;
   this.id = id;
-  this.branding = new EntityLink({ entity: 'Branding' });
   this.rolereason = new RoleReason();
 }
 
@@ -14,7 +12,6 @@ ContactPreference.prototype = new SingleEntity();
 
 ContactPreference.prototype.toArray = function() {
   return {
-    brandingid: this.branding.id,
     role: this.rolereason.role.name,
     reason: this.rolereason.reason.name,
     contactdetailid: this.contactdetail.id,
