fea41e71f38a5035e30499ac1eda58f48a400921
Alex Wyett <alex@carltonsoftware.co.uk>
2017-03-16 14:29:33 +0000

    Added vatband and recharge.

--- src/common/OwnerChargeCode.js
+++ src/common/OwnerChargeCode.js
@@ -1,16 +1,20 @@
 var SingleEntity = require('./SingleEntity');
+var VatBand = require('./VatBand');
 
 function OwnerChargeCode(id) {
   this.path = 'ownerchargecode';
   this.createPath = 'ownerchargecode';
   this.id = id;
+  this.vatband = new VatBand();
 }
 
 OwnerChargeCode.prototype = new SingleEntity();
 OwnerChargeCode.prototype.toArray = function() {
   return {
     ownerchargecode: this.ownerchargecode,
-    description: this.description
+    description: this.description,
+    vatbandid: this.vatband.id,
+    recharge: this.recharge
   };
 };
 OwnerChargeCode.prototype.toString = function() {
