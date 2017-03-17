8471152393400ae05c4d53478ce5d300a0137652
Alex Wyett <alex@carltonsoftware.co.uk>
2017-03-17 01:09:42 +0000

    Added toString to country.

--- src/common/Country.js
+++ src/common/Country.js
@@ -7,12 +7,16 @@ function Country(id) {
 }
 Country.prototype = new SingleEntity();
 Country.prototype.toArray = function() {
-    return {
-        name: this.name,
-        alpha2: this.alpha2,
-        alpha3: this.alpha3,
-        phonecode: this.phonecode
-    };
+  return {
+    name: this.name,
+    alpha2: this.alpha2,
+    alpha3: this.alpha3,
+    phonecode: this.phonecode
+  };
 };
+Country.prototype.toString = function() {
+  return this.name;
+};
+
 
 module.exports = Country;
