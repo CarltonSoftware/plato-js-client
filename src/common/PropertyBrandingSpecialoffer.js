127b0eed64cd1abdee7c75a183fd210d53ca4b8d
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2017-05-10 15:30:39 +0100

    TABS2-1091 update property branding special offers

--- src/common/PropertyBrandingSpecialoffer.js
+++ src/common/PropertyBrandingSpecialoffer.js
@@ -1,17 +1,17 @@
-var Entity = require('./Entity');
+var StaticCollection = require('./StaticCollection');
+var SpecialOffer = require('./SpecialOffer');
 
 function PropertyBrandingSpecialoffer(propertyId, brandId, fromdate, todate) {
   this.path = '/property/' + propertyId + '/branding/' + brandId + '/specialoffer';
   this.params = {
     fromdate: fromdate,
     todate: todate
-  }
+  };
+  this.options = {
+    object: SpecialOffer
+  };
 }
 
-PropertyBrandingSpecialoffer.prototype = new Entity();
-
-PropertyBrandingSpecialoffer.prototype.mutateEntity = function(entity) {
-  return entity;
-};
+PropertyBrandingSpecialoffer.prototype = new StaticCollection();
 
 module.exports = PropertyBrandingSpecialoffer;
