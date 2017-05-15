8bf61c42432c2bde99d4449ebe0a7d4e52612fef
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2017-05-15 10:23:13 +0100

    Add new property comment fields

--- src/common/PropertyComment.js
+++ src/common/PropertyComment.js
@@ -1,23 +1,31 @@
 var SingleEntity = require('./SingleEntity');
+var EntityLink = require('./EntityLink');
 var Joi = require('joi');
 
 function PropertyComment(id) {
   this.createPath = this.path = 'comment';
   this.id = id;
+  this.booking = new EntityLink({
+    entity: 'Booking'
+  });
 }
 PropertyComment.prototype = new SingleEntity();
 PropertyComment.prototype.toArray = function() {
   return {
     comment: this.comment,
     visibletoowner: this.visibletoowner,
-    visibleonweb: this.visibleonweb
+    visibleonweb: this.visibleonweb,
+    bookingid: this.booking && this.booking.id,
+    createddate: this.createddate
   };
 };
 PropertyComment.prototype.validSchema = function() {
   return Joi.object().keys({
     comment: Joi.string().required().label('Comment'),
     visibletoowner: Joi.boolean().label('Visible to owner'),
-    visibleonweb: Joi.boolean().label('Visible on web')
+    visibleonweb: Joi.boolean().label('Visible on web'),
+    booking: Joi.object().label('Booking'),
+    createddate: Joi.date().label('Date')
   });
 };
 
