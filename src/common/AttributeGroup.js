49e630fb7ceb0afb9d0902dca71acbfaed1bdcdc
Alex Wyett <alex@carltonsoftware.co.uk>
2017-06-22 18:07:40 +0100

    Added validSchema to attribute group

--- src/common/AttributeGroup.js
+++ src/common/AttributeGroup.js
@@ -1,18 +1,19 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function AttributeGroup(id) {
   this.path = 'attributegroup';
   this.createPath = 'attributegroup';
   this.id = id;
-}
-AttributeGroup.prototype = new SingleEntity();
 
-AttributeGroup.prototype.toUpdateArray = function() {
-  return {
-    name: this.name
+  this.validSchema = function() {
+    return {
+      name: Joi.string().required().min(1).label('name').description('Attribute group name')
+    }
   };
-};
+}
 
+AttributeGroup.prototype = new SingleEntity();
 AttributeGroup.prototype.toArray = function() {
   return {
     name: this.name
