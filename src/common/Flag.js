769d261cdf3b5c663082f0ea15eaac3f91c571ab
Josh Goodwin <j.goodwin@tabs-software.co.uk>
2017-07-26 09:51:31 +0100

    Allow creating flags

--- src/common/Flag.js
+++ src/common/Flag.js
@@ -1,24 +1,36 @@
 var SingleEntity = require('./SingleEntity');
 var FlagType = require('./FlagType');
+var EntityLink = require('./EntityLink');
 
 function Flag(id) {
   this.path = 'flag';
-  this.createPath = 'flagtype';
+  this.createPath = 'flag';
   this.id = id;
   this.flagtype = new FlagType();
-
+  this.createdBy = new EntityLink({ entity: 'TabsUser' });
+  this.flaggedbybranding = new EntityLink({ entity: 'Branding' });
 }
 
 Flag.prototype = new SingleEntity();
 Flag.prototype.toArray = function() {
-  return {
+  var flag = {
     type: this.type,
     flagtypeid: this.flagtype.id,
-    // createdbyactorid: this.createdby.id,
     // noteid: this.note.id,
-    // flaggedbyactorid: this.flaggedbyactor.id,
-    // flaggedbybrandingid: this.flaggedbybranding.id,
   };
+  if (this.flaggedbybranding) {
+    flag.flaggedbybrandingid = this.flaggedbybranding.id;
+  }
+  if (this.flaggedbyactor) {
+    flag.flaggedbyactorid = this.flaggedbyactor.id;
+  }
+  return flag;
+};
+Flag.prototype.getCreatePath = function() {
+  return 'actor/' + this.parent.id + '/' + this.path;
+};
+Flag.prototype.getUpdatePath = function() {
+  return 'actor/' + this.parent.id + '/' + this.path + '/' + this.id;
 };
 
 module.exports = Flag;
