c8f8bb01c571b281c58d9137273c6c410a850db8
Alex Wyett <alex@carltonsoftware.co.uk>
2017-09-07 09:36:57 +0100

    Added language joi

--- src/common/Language.js
+++ src/common/Language.js
@@ -1,4 +1,5 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function Language(id) {
   this.path = 'language';
@@ -6,12 +7,20 @@ function Language(id) {
   this.id = id;
   this.code = 'EN';
   this.name = 'English';
+
+  this.validSchema = function() {
+    return {
+      name: Joi.string().required().min(1).label('name').description('Language name'),
+      code: Joi.string().required().min(2).max(2).label('code').description('Language code')
+    };
+  };
 }
 
 Language.prototype = new SingleEntity();
 Language.prototype.toArray = function() {
   return {
-    languagecode: this.code
+    name: this.name,
+    code: this.code
   };
 };
 
