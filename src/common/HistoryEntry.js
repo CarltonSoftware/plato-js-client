a0b54a5d0c22c31a22065eb4a39d8fe24577e062
Alex Wyett <alex@carltonsoftware.co.uk>
2017-11-08 14:13:03 +0000

    Added entity for TABS2-2029

--- src/common/HistoryEntry.js
+++ src/common/HistoryEntry.js
@@ -1,5 +1,6 @@
 var SingleEntity = require('./SingleEntity');
 var Actor = require('./Actor');
+var EntityLink = require('./EntityLink');
 
 function HistoryEntry() {
   this.actor = new Actor();
@@ -14,6 +15,17 @@ HistoryEntry.prototype.toArray = function() {
     description: this.description,
     parameters: this.parameters
   };
+};
+
+HistoryEntry.prototype.mutateResponse = function(entity) {
+	if (entity.entity && entity.entity.route) {
+    var e = new EntityLink({
+      entity: entity.entity.type
+    });
+    this[entity.entity.type.toLowerCase()] = e.factory(entity.entity.route);
+  }
+
+  return this.mutateEntity(entity);
 };
 
 module.exports = HistoryEntry;
