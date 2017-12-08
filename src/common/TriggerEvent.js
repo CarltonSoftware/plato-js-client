87949105770d17825ca19ac8f3c4cbf87f1ab07c
Alex Wyett <alex@carltonsoftware.co.uk>
2017-12-08 16:42:36 +0000

    Add valid schema to trigger event.

--- src/common/TriggerEvent.js
+++ src/common/TriggerEvent.js
@@ -1,8 +1,16 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function TriggerEvent(id) {
   this.createPath = this.path = 'triggerevent';
   this.id = id;
+
+  this.validSchema = function() {
+  	return {
+	    triggerevent: Joi.string().label('trigger event'),
+	    eventdescription: Joi.string().label('event description')
+  	}
+  }
 }
 
 TriggerEvent.prototype = new SingleEntity();
