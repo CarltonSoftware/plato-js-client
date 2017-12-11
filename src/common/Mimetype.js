bc624ffbfb2765a0bb41894b0aee6ef273235b9a
Alex Wyett <alex@carltonsoftware.co.uk>
2017-12-11 13:34:14 +0000

    Added more validSchema methods.

--- src/common/Mimetype.js
+++ src/common/Mimetype.js
@@ -1,9 +1,17 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function Mimetype(id) {
   this.path = 'mimetype';
   this.createPath = 'mimetype';
   this.id = id;
+
+  this.validSchema = function() {
+  	return {
+      name: Joi.string().required().label('Name'),
+      shortname: Joi.string().required().label('Short name')
+  	};
+  };
 
   this.toArray = function() {
     return {
