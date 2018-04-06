8b9b9bb4c5b8d2060b1725ff09c68913fa68fe34
alexwyett <alex@carltonsoftware.co.uk>
2018-04-06 16:08:28 +0100

    Added utility methods for lambda functions.

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+let platoClient = require('./index');
+
+module.exports = {
+  SNS: {
+    Message: {
+      getUrl: (message) => {
+        if (message.url.slice(0, 5) !== 'https') {
+          message.url = ['https://', message.url].join('');
+        }
+
+        return message.url;
+      },
+      getProperty: (message) => {
+        var url = module.exports.SNS.Message.getUrl(message);
+        if (message.entity.slice(0, 8) === 'Property') {
+          var parts = url.split('/property/');
+          if (parts.length === 2) {
+            return module.exports.SNS.Message._property(parts.pop().split('/')[0]);
+          }
+        } else {
+          if (typeof message.entity.olddata === 'object') {
+            return module.exports.SNS.Message._property(message.olddata.property.id);
+          } else {
+            return module.exports.SNS.Message._property(message.newdata.property.id);
+          }
+        }
+      },
+      getRoot: (message) => {
+        return [
+          'https://',
+          module.exports.SNS.Message.getUrl(message).split('/')[2]
+        ].join('');
+      },
+      _property: (id) => {
+        return new platoClient.common.Property(
+          parseInt(id)
+        );
+      }
+    }
+  }
+}
---
+++ test/utils.test.js
@@ -0,0 +1,94 @@
+var utils = require('../src/utils.js');
+var data = [{ 
+    entity: 'PropertyAttributeString',
+    id: 2115651,
+    url: 'https://toccl.api.tabs-software.co.uk/v2/property/15213/attribute',
+    action: 'Insert',
+    olddata: '',
+    newdata: { 
+      id: 2115651,
+      attribute: { 
+        id: 721,
+        code: 'ATTR721'
+      },
+      value: 'XXXXXX' 
+    } 
+  }, {
+    entity: 'PropertyAvailability',
+    id: 14421,
+    url: 'toccl.api.tabs-software.co.uk/v2/property/14421',
+    action: 'Insert',
+    time: '2018-03-13 10:42:32',
+    olddata: '',
+    newdata: { 
+      id: 14421,
+      type: 'property',
+      tabspropref: '40911',
+      name: 'Yr Hen Dy Coets'
+    }
+  }, { 
+    entity: 'PropertyBranding',
+    id: 22913,
+    url: 'https://toccl.api.tabs-software.co.uk/v2/property/13844/branding/22913',
+    action: 'Update',
+    time: '2017-11-24 12:48:33',
+    olddata: { 
+      id: 22913,
+      branding: '/v2/branding/22',
+      primarybranding: true,
+      brandinggroup: '/v2/brandinggroup/15',
+      bookingbrand: '/v2/property/13844/bookingbrand/15880',
+      primarybookingbrand: true,
+      marketingbrand: '/v2/property/13844/marketingbrand/22841',
+      status: { 
+        id: 2,
+        name: 'New',
+        allowbooking: false,
+        allowoverride: false,
+        priority: 8,
+        allowbalance: true 
+      },
+      statusreason: { id: 2, reason: 'New' },
+      promote: false,
+      allowbookingonwebuntil: '2019-01-04',
+      showpricingonwebuntil: '2019-01-04'
+    },
+    newdata: {
+      id: 22913,
+      branding: '/v2/branding/22',
+      primarybranding: true,
+      brandinggroup: '/v2/brandinggroup/15',
+      bookingbrand: '/v2/property/13844/bookingbrand/15880',
+      primarybookingbrand: true,
+      marketingbrand: '/v2/property/13844/marketingbrand/22841',
+      status: { 
+        id: 1,
+        name: 'Live',
+        allowbooking: true,
+        allowoverride: false,
+        priority: 10,
+        allowbalance: true
+      },
+      statusreason: { id: 1, reason: 'Live' },
+      promote: false,
+      allowbookingonwebuntil: '2019-01-04',
+      showpricingonwebuntil: '2019-01-04'
+    }
+  }
+];
+
+describe('Utilities test', function() {
+  describe('SNS', function() {
+    for (var i in data) {
+      it('utils.SNS.Message.getUrl should return a url', function() {
+        console.assert(utils.SNS.Message.getUrl(data[i]) === data[i].url);
+      });
+      it('utils.SNS.Message.getRoot should return a url', function() {
+        console.assert(utils.SNS.Message.getRoot(data[i]) === 'https://toccl.api.tabs-software.co.uk');
+      });
+      it('utils.SNS.Message.getProperty should return a property', function() {
+        console.assert(utils.SNS.Message.getProperty(data[i]) instanceof new require('../src/common/Property.js'));
+      });
+    }
+  });
+});
