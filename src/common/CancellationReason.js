b3c6b09204f459f53d272add0939cee751bfc9ba
Josh Goodwin <j@joshuagoodw.in>
2018-10-14 12:51:33 +0100

    TABS2-3635 add new booleans to CancellationReason

--- src/common/CancellationReason.js
+++ src/common/CancellationReason.js
@@ -10,13 +10,27 @@ CancellationReason.prototype = new SingleEntity();
 
 CancellationReason.prototype.validSchema = function() {
   return {
-    cancellationreason: Joi.string().required().label('Cancellation Reason')
+    cancellationreason: Joi.string().required().label('Cancellation Reason'),
+    covered: Joi.boolean().label('Covered by cancellation protection scheme'),
+    agencybooking: Joi.boolean().label('Applies to agency bookings'),
+    ownerbooking: Joi.boolean().label('Owner bookings'),
+    customerbooking: Joi.boolean().label('Customer bookings'),
+    potentialbooking: Joi.boolean().label('Potential bookings'),
+    provisionalbooking: Joi.boolean().label('Provisional bookings'),
+    confirmedbooking: Joi.boolean().label('Confirmed bookings'),
   };
 };
 
 CancellationReason.prototype.toArray = function() {
   return {
-    cancellationreason: this.cancellationreason
+    cancellationreason: this.cancellationreason,
+    covered: this.covered,
+    agencybooking: this.agencybooking,
+    ownerbooking: this.ownerbooking,
+    customerbooking: this.customerbooking,
+    potentialbooking: this.potentialbooking,
+    provisionalbooking: this.provisionalbooking,
+    confirmedbooking: this.confirmedbooking,
   };
 };
 
