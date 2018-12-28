9eaaa4c24b54e35cefaac609b005a076e4029770
Mark <mark@marksharratt.net>
2018-12-28 11:03:37 +0000

    TABS2-4135. Temp relaxation of keycheck validation

--- src/common/KeyCheck.js
+++ src/common/KeyCheck.js
@@ -36,13 +36,13 @@ KeyCheck.prototype.toArray = function() {
 KeyCheck.prototype.validSchema = function() {
   return Joi.object().keys({
     type: Joi.string().valid(['KeyCheckOut', 'KeyCheckIn']).required(),
-    checkdatetime: Joi.date().label('Date and time'),
+    checkdatetime: Joi.string().label('Date and time'),
     notes: Joi.string().empty('').label('Notes'),
     tabsuser: Joi.object().label('Tabs user'),
     keycheckreason: Joi.object().label('Reason'),
     expectedbackdatetime: Joi.when('type', {
       is: 'KeyCheckOut',
-      then: Joi.date().empty(''),
+      then: Joi.string().empty(''),
       otherwise: Joi.forbidden()
     }).label('Expected back date and time'),
     actor: Joi.object().empty('').label('Actor'),
