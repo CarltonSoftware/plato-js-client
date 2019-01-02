63bf02af4c56a62c140dcc43278412b88ba7220b
Alex <alex.wyett@gmail.com>
2019-01-02 17:00:57 +0000

    Layout

--- src/common/ChargingPeriod.js
+++ src/common/ChargingPeriod.js
@@ -1,28 +1,26 @@
 var SingleEntity = require('./SingleEntity');
 var Joi = require('joi');
 
-
 function ChargingPeriod(id) {
-      this.path = 'chargingperiod';
-      this.createPath = 'chargingperiod';
-      this.id = id;
+  this.path = 'chargingperiod';
+  this.createPath = 'chargingperiod';
+  this.id = id;
 
-      this.validSchema = function() {
-            return {
-                chargingperiod: Joi.string().required().label('chargingperiod'),
-                description: Joi.string().label('description')
-            };
-      };
+  this.validSchema = function() {
+    return {
+      chargingperiod: Joi.string().required().label('chargingperiod'),
+      description: Joi.string().label('description')
+    };
+  };
 }
 
 ChargingPeriod.prototype = new SingleEntity();
 
 ChargingPeriod.prototype.toArray = function() {
-    return {
-            "chargingperiod": this.chargingperiod,
-            "description": this.description
-    };
+  return {
+    "chargingperiod": this.chargingperiod,
+    "description": this.description
+  };
 };
-
 
 module.exports = ChargingPeriod;
