50b04f8b8ada4d704b726c344e6ae9f2926cc223
Alex <alex.wyett@gmail.com>
2019-01-02 11:59:21 +0000

    Add validSchema to exchange rate issue type

--- src/common/ExchangeRateIssueType.js
+++ src/common/ExchangeRateIssueType.js
@@ -1,8 +1,16 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function ExchangeRateIssueType(id) {
   this.path = this.createPath = 'exchangerateissuetype';
   this.id = id;
+
+  this.validSchema = function() {
+    return {
+      exchangerateissuetype: Joi.string().label('type'),
+      description: Joi.string().label('description')
+    }
+  };
 }
 
 ExchangeRateIssueType.prototype = new SingleEntity();
