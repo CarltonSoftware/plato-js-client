d0673af48cfec06d58c75027a9ff2f48d74ba2f6
Alex <alex.wyett@gmail.com>
2019-01-03 17:00:25 +0000

    Add validSchema to DepositAmount

--- src/common/DepositAmount.js
+++ src/common/DepositAmount.js
@@ -2,6 +2,7 @@ var SingleEntity = require('./SingleEntity');
 var Branding = require('./Branding');
 var Currency = require('./Currency');
 var DepositAmountType = require('./DepositAmountType');
+var Joi = require('joi');
 
 function DepositAmount(id) {
   this.path = 'depositamount';
@@ -10,6 +11,37 @@ function DepositAmount(id) {
   this.branding = new Branding();
   this.currency = new Currency();
   this.depositamounttype = new DepositAmountType();
+
+  this.validSchema = function() {
+    var s = {
+      type: Joi.string().required().label('type').allow(['General', 'Property', 'Booking', 'Branding']),
+      name: Joi.string().required().label('name'),
+      description: Joi.string().required().label('description'),
+      currency: Joi.object().required().label('currency'),
+      pricingperiod: Joi.object().required().label('pricing period'),
+      depositamounttype: Joi.object().required().label('deposit amount type'),
+      amount: Joi.string().required().label('amount'),
+      decimalplaces: Joi.number().required().label('decimal places'),
+      usebrochureprice: Joi.boolean().label('use brochure price?'),
+      plusadditionalextras: Joi.boolean().label('plus additional extras?')
+    };
+
+    if (['Branding', 'Property'].indexOf(this.type) >= 0) {
+      s.fromdate = Joi.date().required().label('from date');
+      s.todate = Joi.date().required().label('from date');
+      s.minimumamount = Joi.number().required().label('minimum amount');
+    }
+
+    if (this.type === 'Property') {
+      s.property = Joi.object().required().label('Property');
+    }
+
+    if (this.type === 'Branding') {
+      s.branding = Joi.object().required().label('Branding');
+    }
+
+    return s;
+  }
 }
 DepositAmount.prototype = new SingleEntity();
 
