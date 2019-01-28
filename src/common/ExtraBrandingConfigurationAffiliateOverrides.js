4438554b36330766ed64dbca336be345511ae7cd
wgeorgecrisan <g.crisan@tabs-software.co.uk>
2019-01-28 14:54:10 +0000

    TABS2-3926

---
+++ src/common/ExtraBrandingConfigurationAffiliateOverrides.js
@@ -0,0 +1,25 @@
+var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
+
+function ExtraBrandingConfigurationAffiliateOverride(id) {
+  this.path = 'affiliateoverride';
+  this.createPath = 'affiliateoverride';
+  this.id = id;
+
+  this.validSchema = function() {
+    return {
+      compulsory: Joi.bool().required().label('compulsory'),
+    };
+  };
+}
+
+ExtraBrandingConfigurationAffiliateOverride.prototype = new SingleEntity();
+
+ExtraBrandingConfigurationAffiliateOverride.prototype.toArray = function() {
+  return {
+    "compulsory": this.compulsory
+    //add more config options later
+  };
+};
+
+module.exports = ExtraBrandingConfigurationAffiliateOverride;
