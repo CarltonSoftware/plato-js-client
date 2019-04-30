24641ebbe351f08413750c002dbe69a5d80eb581
wgeorgecrisan <g.crisan@tabs-software.co.uk>
2019-04-30 10:12:08 +0100

    TABS2-1666

---
+++ src/common/TabsUserGroupMember.js
@@ -0,0 +1,27 @@
+var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
+
+
+function TabsUserGroupMember(id) {
+    this.path = 'member';
+    this.createPath = this.path;
+    this.id = id;
+
+    this.validSchema = function() {
+        return {
+            userid: Joi.string().required().label('Tabs user id'),
+            inactive: Joi.boolean().optional().label('Inactive')
+        };
+    }
+}
+
+TabsUserGroupMember.prototype = new SingleEntity();
+
+TabsUserGroupMember.prototype.toArray = function() {
+    return {
+        userid: this.userid.id,
+        inactive: this.inactive
+    }
+}
+
+module.exports = TabsUserGroupMember;
