64e6f9c0462e4245a89b29532e689bfd9305dfbb
Chris Dobson <c.dobson@tocc.co.uk>
2019-08-19 19:34:37 +0100

    PMS stuff

--- src/common/Supplier.js
+++ src/common/Supplier.js
@@ -1,8 +1,21 @@
+var Collection = require('./Collection');
 var Actor = require('./Actor');
+var SupplierHoliday = require('./SupplierHoliday');
+var SupplierWorkingDay = require('./SupplierWorkingDay');
 
 function Supplier() {
   this.path = 'supplier';
   this.createPath = 'supplier';
+
+  this.holidays = new Collection({
+    object: SupplierHoliday,
+    parent: this
+  });
+
+  this.workingdays = new Collection({
+    object: SupplierWorkingDay,
+    parent: this
+  });
 
   Actor.apply(this, arguments);
 }
