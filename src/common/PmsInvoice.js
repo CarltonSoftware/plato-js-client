d170dd654eb2bbcd3b2a035cd260b2730d3c1c5c
Chris Dobson <c.dobson@tocc.co.uk>
2019-11-27 14:23:38 +0000

    sorry bev

---
+++ src/common/PmsInvoice.js
@@ -0,0 +1,25 @@
+var client = require('./platoJsClient').getInstance();
+var SingleEntity = require('./SingleEntity');
+
+PmsInvoice.prototype = new SingleEntity();
+
+function PmsInvoice(id) {
+  this.path = 'pmsinvoice';
+  this.createPath = this.path;
+  this.id = id;
+}
+
+PmsInvoice.prototype.toArray = function() {
+  return {
+    ownerid: this.ownerid,
+    chargeids: this.chargeids,
+  };
+};
+
+PmsInvoice.prototype.preview = function(ownerId, chargeIds, addendum, $workDoneDate) {
+  return client.get(
+    `${this.path}preview?ownerid=${ownerId}&chargeids=${chargeIds}&addendum=${encodeURIComponent(addendum)}&workdonedate=${$workDoneDate}`
+  );
+};
+
+module.exports = PmsInvoice;
