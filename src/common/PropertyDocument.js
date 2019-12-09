f195d1731192d8147583624886fea388d8543b44
wgeorgecrisan <g.crisan@tabs-software.co.uk>
2019-12-09 13:22:25 +0000

    feat: inspectionid to PropertyDocument TABS2-2320

--- src/common/PropertyDocument.js
+++ src/common/PropertyDocument.js
@@ -7,13 +7,21 @@ function PropertyDocument(propertyId, id) {
   this.createPath = 'property/' + propertyId + '/document';
   this.image = new Image();
   this.document = new Document();
+  this.inspectionId = false;
   this.id = id;
 }
 PropertyDocument.prototype = new SingleEntity();
 PropertyDocument.prototype.toCreateArray = function() {
-  return {
+
+  let fileds = {
     documentid: this.image.id || this.document.id
   };
+
+  if(this.inspectionId) {
+    fileds.inspectionid = this.inspectionId;
+  }
+
+  return fileds;
 };
 
 module.exports = PropertyDocument;
