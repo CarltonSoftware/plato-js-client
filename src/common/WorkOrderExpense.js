fa0f54fa46874f4b64ed005e44677f24409f92d0
Chris Dobson <c.dobson@tocc.co.uk>
2019-12-15 11:46:38 +0000

    WorkOrderExpense tweak

--- src/common/WorkOrderExpense.js
+++ src/common/WorkOrderExpense.js
@@ -49,7 +49,7 @@ WorkOrderExpense.prototype.toArray = function() {
   if (this.parent.type === 'Template' || (this.parent.type === 'Instance' && !this.parent.workordertemplate)) {
     if (this.amountnetestimate) {obj.amountnetestimate = this.amountnetestimate;}
     if (this.chargingperiodsestimate) {obj.chargingperiodsestimate = this.chargingperiodsestimate;}
-    if (this.amountlimittype.id >= 0) {obj.amountlimittypeid = this.amountlimittype.id;}
+    if (this.amountlimittype !== undefined && this.amountlimittype.id >= 0) {obj.amountlimittypeid = this.amountlimittype.id;}
     if (this.amountnetlimit) {obj.amountnetlimit = this.amountnetlimit;}
   }
   
