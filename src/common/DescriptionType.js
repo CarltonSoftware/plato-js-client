8813ead41acb098c5d06594d5adebe4c3618a464
wgeorgecrisan <g.crisan@tabs-software.co.uk>
2019-12-17 13:37:12 +0000

    TABS2-5180 fix: description type screen

--- src/common/DescriptionType.js
+++ src/common/DescriptionType.js
@@ -11,28 +11,38 @@ function DescriptionType(id) {
 
 DescriptionType.prototype = new SingleEntity();
 DescriptionType.prototype.toArray = function() {
-  return {
+
+  
+
+  var data = {
     code: this.code,
     name: this.name,
     description: this.description,
-    encoding: this.encoding.encoding,
     minimumlength: this.minimumlength,
     maximumlength: this.maximumlength,
     donotmodify: this.donotmodify,
     sortorder: this.order
   };
+
+  if(typeof this.encoding === 'object') {
+    data.encoding = this.encoding.encoding;
+  } else if (typeof this.encoding === 'string' && this.encoding.length > 3) {
+    data.encoding = this.encoding;
+  }
+
+  return data;
 };
 
 DescriptionType.prototype.validSchema = function() {
   return {
-    code: Joi.string().min(2).max(8),
-    name: Joi.string(),
-    description: Joi.string(),
-    encoding: Joi.object().required(),
+    code: Joi.string().min(2).max(8).label('Code'),
+    name: Joi.string().label('Name'),
+    description: Joi.string().label('Description'),
+    encoding: Joi.string().valid('Plain','HTML','XML').required().label('Encoding - Plain, HTML, XML'),
     minimumlength: Joi.number().label('Minimum length'),
     maximumlength: Joi.number().label('Maximum length'),
-    donotmodify: Joi.boolean(),
-    order: Joi.number(),
+    donotmodify: Joi.boolean().label('Do not modify'),
+    order: Joi.number().label('Sort order'),
   };
 };
 
