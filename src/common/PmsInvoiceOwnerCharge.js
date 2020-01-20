97433c376a86d1565afe7ee16f3a0f89e278cc71
Chris Dobson <c.dobson@tocc.co.uk>
2020-01-20 19:36:03 +0000

    add PmsInvoiceOwnerCharge obj

---
+++ src/common/PmsInvoiceOwnerCharge.js
@@ -0,0 +1,20 @@
+var client = require('./platoJsClient').getInstance();
+var SingleEntity = require('./SingleEntity');
+var OwnerCharge = require('./OwnerCharge');
+
+PmsInvoiceOwnerCharge.prototype = new SingleEntity();
+
+function PmsInvoiceOwnerCharge(id) {
+  this.path = 'pmsinvoice';
+  this.createPath = this.path;
+  this.ownercharge = new OwnerCharge();
+  this.id = id;
+}
+
+PmsInvoiceOwnerCharge.prototype.toArray = function() {
+  return {
+    ownerchargeid: this.ownercharge.id
+  };
+};
+
+module.exports = PmsInvoiceOwnerCharge;
--- src/index.js
+++ src/index.js
@@ -280,6 +280,7 @@ module.exports.common.ManagedActivity = require('./common/ManagedActivity');
 module.exports.common.ManagedService = require('./common/ManagedService');
 module.exports.common.ManagedActivityService = require('./common/ManagedActivityService');
 module.exports.common.PmsInvoice = require('./common/PmsInvoice');
+module.exports.common.PmsInvoiceOwnerCharge = require('./common/PmsInvoiceOwnerCharge');
 
 // Note Related Objects
 module.exports.common.Note = require('./common/Note');
