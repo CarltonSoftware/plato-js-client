8f291ab3380137d8d6fddc61fc9e7478f0057c78
Ryan Knights <hello@ryanknights.co.uk>
2020-03-09 11:20:01 +0000

    TABS2-5268 feat: add extras collection to bookingreason entity

--- src/common/BookingReason.js
+++ src/common/BookingReason.js
@@ -1,10 +1,16 @@
 var SingleEntity = require('./SingleEntity');
+var Extra = require('./Extra');
+var Collection = require('./Collection');
 var Joi = require('joi');
 
 function BookingReason(id) {
   this.path = 'bookingreason';
   this.createPath = 'bookingreason';
   this.id = id;
+  this.extras = new Collection({
+    object: Extra,
+    path: 'extra',
+  });
 }
 BookingReason.prototype = new SingleEntity();
 
