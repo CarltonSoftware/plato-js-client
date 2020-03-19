d9e82882efe097b16681def9db53ed371f62c089
Ryan Knights <hello@ryanknights.co.uk>
2020-03-19 09:58:07 +0000

    TABS2-3989 feat: add updatepropertypricing & updateagreedpropertypricing to PricingGroupYear & PricingGroupYearPriceBand entities

--- src/common/PricingGroupYear.js
+++ src/common/PricingGroupYear.js
@@ -16,12 +16,22 @@ function PricingGroupYear(id) {
 
 PricingGroupYear.prototype = new SingleEntity();
 PricingGroupYear.prototype.toArray = function() {
-  return {
+  var fields = {
     year: this.year,
     amount1: this.amount1,
     percentage: this.percentage,
     amount2: this.amount2,
   };
+
+  if (this.updatepropertypricing) {
+    fields.updatepropertypricing = this.updatepropertypricing;
+  }
+
+  if (this.updateagreedpropertypricing) {
+    fields.updateagreedpropertypricing = this.updateagreedpropertypricing;
+  }
+
+  return fields;
 };
 PricingGroupYear.prototype.validSchema = function() {
   return {
@@ -29,6 +39,8 @@ PricingGroupYear.prototype.validSchema = function() {
     amount1: Joi.number().empty('').label('amount to add before adding percentage (optional)'),
     percentage: Joi.number().empty('').label('percentage to add (optional)'),
     amount2: Joi.number().empty('').label('amount to add after adding percentage (optional)'),
+    updatepropertypricing: Joi.boolean().optional().label('existing property pricing for the year should be updated'),
+    updateagreedpropertypricing: Joi.boolean().optional().label('agreed pricing should be updated'),
   };
 };
 
--- src/common/PricingGroupYearPriceBand.js
+++ src/common/PricingGroupYearPriceBand.js
@@ -11,12 +11,22 @@ function PricingGroupYearPriceBand(id) {
 
 PricingGroupYearPriceBand.prototype = new SingleEntity();
 PricingGroupYearPriceBand.prototype.toArray = function() {
-  return {
+  var fields = {
     pricebandid: this.priceband.id,
     amount1: this.amount1,
     percentage: this.percentage,
     amount2: this.amount2,
   };
+
+  if (this.updatepropertypricing) {
+    fields.updatepropertypricing = this.updatepropertypricing;
+  }
+
+  if (this.updateagreedpropertypricing) {
+    fields.updateagreedpropertypricing = this.updateagreedpropertypricing;
+  }
+
+  return fields;
 };
 PricingGroupYearPriceBand.prototype.validSchema = function() {
   return {
@@ -24,6 +34,8 @@ PricingGroupYearPriceBand.prototype.validSchema = function() {
     amount1: Joi.number().empty('').label('amount to add before adding percentage (optional)'),
     percentage: Joi.number().empty('').label('percentage to add (optional)'),
     amount2: Joi.number().empty('').label('amount to add after adding percentage (optional)'),
+    updatepropertypricing: Joi.boolean().optional().label('existing property pricing for the year should be updated'),
+    updateagreedpropertypricing: Joi.boolean().optional().label('agreed pricing should be updated'),
   };
 };
 
