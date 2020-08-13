5134be8922c07bcf918c451a9b1114ce74b222c1
Alex <alex.wyett@gmail.com>
2020-08-13 11:43:52 +0100

    TABS2-5594: Fix property link to use parent/child properties

--- src/common/PropertyLink.js
+++ src/common/PropertyLink.js
@@ -6,19 +6,20 @@ function PropertyLink(id) {
   this.path = 'link';
   this.createPath = 'link';
   this.id = id;
-  this.virtualproperty = new EntityLink({ entity: 'Property' });
+  this.childproperty = new EntityLink({ entity: 'Property' });
+  this.parentproperty = new EntityLink({ entity: 'Property' });
 }
 PropertyLink.prototype = new SingleEntity();
 
 PropertyLink.prototype.toArray = function() {
   return {
-    childpropertyid: this.virtualproperty.id
+    childpropertyid: this.childproperty.id
   };
 };
 
 PropertyLink.prototype.validSchema = function() {
   return Joi.object().keys({
-    virtualproperty: Joi.object().optional().label('Linked Property')
+    childproperty: Joi.object().optional().label('Linked Property')
   });
 };
 
