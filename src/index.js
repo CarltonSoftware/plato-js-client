c2cb7d37bad046ece9587da580cb6eb884e79ca3
Alex <alex.wyett@gmail.com>
2020-08-19 11:36:34 +0100

    TABS2-5603: Add property parking permit entities to Property

--- src/index.js
+++ src/index.js
@@ -210,6 +210,8 @@ module.exports.common.SpecialOfferDiscountExtra = require('./common/SpecialOffer
 module.exports.common.SpecialOfferDiscountedExtra = require('./common/SpecialOfferDiscountedExtra');
 module.exports.common.PropertyApproval = require('./common/PropertyApproval');
 module.exports.common.PropertyLink = require('./common/PropertyLink');
+module.exports.common.ParkingPermit = require('./common/ParkingPermit');
+module.exports.common.ParkingPermitHolidayPeriod = require('./common/ParkingPermitHolidayPeriod');
 
 // Key Related Objects
 module.exports.common.Keyy = require('./common/Keyy');
