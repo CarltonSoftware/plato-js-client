1439c5fbb862cf0c54bebaee779a0f81fef14d16
Alex <alex.wyett@gmail.com>
2020-08-26 16:38:41 +0100

    linting.

--- src/common/Vehicle.js
+++ src/common/Vehicle.js
@@ -1,27 +1,23 @@
 var SingleEntity = require('./SingleEntity');
 var Joi = require('joi');
 
-
 function Vehicle(id) {
   this.path = 'vehicle';
   this.createPath = this.path;
   this.id = id;
 
   this.validSchema = function() {
-
-      return {
-        make:  Joi.string().label('make'),
-        model: Joi.string().label('model'),
-        registration: Joi.string().min(4).label('registration'),
-        colour: Joi.string().allow("").label('colour'),
-        comments: Joi.string().allow("").label('comments'),
-    } 
-  }
+    return {
+      make:  Joi.string().label('make'),
+      model: Joi.string().label('model'),
+      registration: Joi.string().min(4).label('registration'),
+      colour: Joi.string().allow("").label('colour'),
+      comments: Joi.string().allow("").label('comments'),
+    }
+  };
 }
 
 Vehicle.prototype = new SingleEntity();
-
-
 Vehicle.prototype.toArray = function() {
   return {
     make: this.make,
@@ -29,7 +25,7 @@ Vehicle.prototype.toArray = function() {
     registration: this.registration,
     colour: this.colour,
     comments: this.comments
-  }
+  };
 }
 
 module.exports = Vehicle;
