7029cf38e1d1f826996b0019d605f81392415458
Chris Dobson <c.dobson@tocc.co.uk>
2020-11-06 16:34:46 +0000

    TABS2-5725 fix: add Joi shiz to PropertyEventCategory

--- src/common/PropertyEventCategory.js
+++ src/common/PropertyEventCategory.js
@@ -1,8 +1,16 @@
 var SingleEntity = require('./SingleEntity');
+var Joi = require('joi');
 
 function PropertyEventCategory(id) {
   this.path = this.createPath = 'propertyeventcategory';
   this.id = id;
+
+  this.validSchema = function() {
+    return {
+      name: Joi.string().required().label('name'),
+      description: Joi.string().required().label('description'),
+    };
+  }
 }
 PropertyEventCategory.prototype = new SingleEntity();
 
