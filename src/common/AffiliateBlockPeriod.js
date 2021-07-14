44a218604600836568ee4bdfd20fadca3eba51ec
Alex <alex.wyett@gmail.com>
2021-07-14 16:36:37 +0100

    DEV-172 feat: Add block property to BlockPeriods

--- src/common/AffiliateBlockPeriod.js
+++ src/common/AffiliateBlockPeriod.js
@@ -14,6 +14,7 @@ AffiliateBlockPeriod.prototype.validSchema = function() {
   return Joi.object().keys({
     fromdate: Joi.date(),
     todate: Joi.date(),
+    block: Joi.boolean(),
     bookingbrand: Joi.object()
   });
 };
@@ -22,6 +23,7 @@ AffiliateBlockPeriod.prototype.toArray = function() {
   return {
     fromdate: this.fromdate,
     todate: this.todate,
+    block: this.block,
     bookingbrandid: this.bookingbrand.id
   };
 };
