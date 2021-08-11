162a9af3e584b856322a567ed13cfd9296d50e6f
Samantha Eastwood <s.eastwood@originalcottages.co.uk>
2021-08-11 11:54:31 +0100

    DEV-662 feat: fix typo on numOccurences

--- src/common/WorkOrder.js
+++ src/common/WorkOrder.js
@@ -193,8 +193,8 @@ WorkOrder.prototype.toArray = function() {
   return arr;
 };
 
-WorkOrder.prototype.recur = function(numOccurances=1) {
-  return this.createPromiseResult([this.path, this.id, 'recur'].join('/'), {'numOccurances': numOccurances});
+WorkOrder.prototype.recur = function(numOccurences=1) {
+  return this.createPromiseResult([this.path, this.id, 'recur'].join('/'), {'numOccurences': numOccurences});
 };
 
 WorkOrder.prototype.chargeowner = function(data) {
