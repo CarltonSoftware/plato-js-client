bc6ca6bb8089501b46817881409d397bd82a6d76
Alex <alex.wyett@gmail.com>
2021-08-18 13:22:53 +0100

    DEV-172 feat: Start adding new channel manager entities.

---
+++ src/common/BlockPeriod.js
@@ -0,0 +1,37 @@
+var SingleEntity = require('./SingleEntity');
+var BlockPeriodChannelBookingBrand = require('./BlockPeriodChannelBookingBrand');
+var Collection = require('./Collection');
+var Joi = require('joi');
+
+function BlockPeriod(id) {
+  this.path = 'room';
+  this.createPath = this.path;
+  this.id = id;
+
+  this.channelbookingbrand = new Collection({
+    object: BlockPeriodChannelBookingBrand,
+    path: 'channelbookingbrand',
+    parent: this
+  });
+
+  this.validSchema = function() {
+    return {
+      name: Joi.string().required().label('Name').description('Name of the block period'),
+      description: Joi.string().required().label('Description').description('Description of the block period'),
+      fromdate: Joi.string().required().label('From date').description('Starting date of the period that the block period applies too.'),
+      todate: Joi.string().required().label('To date').description('End date of the period that the block period applies too.')
+    }
+  };
+}
+
+BlockPeriod.prototype = new SingleEntity();
+BlockPeriod.prototype.toArray = function() {
+  return {
+    name: this.name,
+    description: this.description,
+    fromdate: this.fromdate,
+    todate: this.todate
+  };
+};
+
+module.exports = BlockPeriod;
