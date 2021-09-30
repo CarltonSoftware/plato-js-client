238b3c17e66bb9065c610d7910c92426e5511362
Pete Lawrence <petejlawrence@icloud.com>
2021-09-30 15:13:59 +0100

    DEV-47 Populate incontactcontactid and incontactmasterid fields on Contact::create()

--- src/common/Contact.js
+++ src/common/Contact.js
@@ -64,7 +64,9 @@ Contact.prototype.toCreateArray = function() {
     templateentityid: this.templateentityid,
     templateid: this.template.id,
     document_documentid: this.document_documentid,
-    comments: this.comments
+    comments: this.comments,
+    incontactcontactid: this.incontactcontactid,
+    incontactmasterid: this.incontactmasterid
   };
   if (this.contactreason && this.contactreason.id) {
     c.contactreasonid = this.contactreason.id;
