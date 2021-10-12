259c261fe3452f6cc680f859dc9965660bcbf1cb
Thomas Whittington <t.whittington@originalcottages.co.uk>
2021-10-12 12:02:55 +0100

    DEV-755 Add TemplateSpecialOffer to CancellationReasonGroup
    -Almost finished.
    -Need to apply the filter collection to the edit panel.
    -Edit panel won't show Joi.object() for some reason.

--- src/common/CancellationReasonGroup.js
+++ src/common/CancellationReasonGroup.js
@@ -1,14 +1,17 @@
 var SingleEntity = require('./SingleEntity');
 var Joi = require('joi');
+var FilterCollection = require('./FilterCollection');
+var SpecialOffer = require('./SpecialOffer');
 
 function CancellationReasonGroup(id) {
   this.path = this.createPath = 'cancellationreasongroup';
   this.id = id;
- 
+  this.templatespecialoffer = new SpecialOffer();
   this.validSchema = function() {
     return {
-      groupname: Joi.string().required().label('CancellationReason group')
-    }
+      groupname: Joi.string().required().label('CancellationReason group'),
+      templatespecialoffer: Joi.object().optional().label('Template Special Offer'),
+    };
   };
 }
 CancellationReasonGroup.prototype = new SingleEntity();
@@ -16,7 +19,25 @@ CancellationReasonGroup.prototype = new SingleEntity();
 CancellationReasonGroup.prototype.toArray = function() {
   return {
     groupname: this.groupname,
+    templatespecialoffer: this.templatespecialoffer
   };
+};
+
+CancellationReasonGroup.prototype.toUpdateArray = function() {
+  return {
+    groupname: this.groupname,
+    templatespecialofferid: this.templatespecialoffer ? this.templatespecialoffer.id : 0
+  };
+};
+
+CancellationReasonGroup.prototype.getTemplateTypeCancellationsSpecialOffers = function(){
+  var specialoffer = new FilterCollection({
+    object: SpecialOffer,
+    path: 'specialoffer'
+  });
+  specialoffer.addFilter('templatetype', 'Cancellation');
+  console.log(specialoffer);
+  return specialoffer.fetch();
 };
 
 module.exports = CancellationReasonGroup;
