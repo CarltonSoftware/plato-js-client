79c60dc52370ff1930231e53b4d7818a676cae87
Simon <s.foster@tabs-software.co.uk>
2022-05-27 12:23:49 +0100

    Fix: don't send amount if it isn't set to SpecialOfferBranding

--- src/common/SpecialOfferBranding.js
+++ src/common/SpecialOfferBranding.js
@@ -17,4 +17,17 @@ SpecialOfferBranding.prototype.toArray = function() {
   };
 };
 
+SpecialOfferBranding.prototype.toCreateArray = function() {
+  // don't send the amount if it isn't set as the API misinterprets this as zero
+  if (this.amount) {
+    return this.toArray();
+  }
+
+  return {
+    brandingid: this.branding.id,
+    description: this.description,
+    active: this.active,
+  };
+};
+
 module.exports = SpecialOfferBranding;
